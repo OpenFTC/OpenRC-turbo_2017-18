f7a30721bb70d9629c81ee91755293a05e1b5348
meow10811 <10765599+meow10811@users.noreply.github.com>
2017-11-19 22:28:29 -0500

    Continued adding AutoBase functions, created a test Auto program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBase.java
@@ -25,6 +25,9 @@ public class AutoBase extends LinearOpMode {
 
     }
 
+    /**
+     * 
+     */
     public void openClaw() {
 
         robot.clawLeft.setPosition(RevbotValues.LEFT_CLAW_OPENED_VALUE);
@@ -40,6 +43,82 @@ public class AutoBase extends LinearOpMode {
     }
 
     public void initFondler() {
+        robot.fondler.setPosition(RevbotValues.FONDLER_CENTER_VALUE);
+    }
 
+    public void strafeLeft(double power) {
+        robot.strafe.setPower(power);
+    }
+
+    public void strafeRight(double power) {
+        strafeLeft(power);
+    }
+
+    public void stopStrafing() {
+        robot.strafe.setPower(0);
+    }
+    
+    /**
+     * Strafe left (using motor "strafe")
+     * @param seconds seconds to wait
+     * @param power power, between 0 and 1
+     */
+    public void strafeLeft(double seconds, double power) {
+        robot.strafe.setPower(power);
+        sleep((long) seconds*1000);
+        robot.strafe.setPower(0);
+    }
+
+    /**
+     * Strafe right (using motor "strafe")
+     * @param seconds seconds to wait
+     * @param power power, between 0 and 1
+     */
+    public void strafeRight(double seconds, double power){
+        strafeLeft(seconds, -power);
+    }
+
+    /**
+     * Drive forward (using motors "leftDrive" and "rightDrive)
+     * @param seconds seconds to wait
+     * @param power power, between 0 and 1
+     */
+    public void forward(double seconds, double power) {
+        robot.leftDrive.setPower(power);
+        robot.rightDrive.setPower(power);
+        sleep((long) seconds*1000);
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
+    }
+
+    /**
+     * Drive backward (using motors "leftDrive" and "rightDrive)
+     * @param seconds seconds to wait
+     * @param power power, between 0 and 1
+     */
+    public void backward(double seconds, double power){
+        forward(seconds, -power);
+    }
+
+    /**
+     * Turn left (using motors "leftDrive" and "rightDrive)
+     * @param seconds seconds to wait
+     * @param power power, between 0 and 1
+     */
+    public void turnLeft(double seconds, double power) {
+        robot.leftDrive.setPower(-power);
+        robot.rightDrive.setPower(power);
+        sleep((long) seconds*1000);
+        robot.leftDrive.setPower(0);
+        robot.rightDrive.setPower(0);
+    }
+
+    /**
+     * Turn right (using motors "leftDrive" and "rightDrive)
+     * @param seconds seconds to wait
+     * @param power power, between 0 and 1
+     */
+    public void turnRight(double seconds, double power) {
+        turnLeft(seconds, -power);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoRedTop.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Auto Red Top.
+ */
+
+@SuppressWarnings("unused")
+@Autonomous(name = "Auto Red Top", group = "auto")
+public class AutoRedTop extends AutoBase {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+
+    }
+
+}
